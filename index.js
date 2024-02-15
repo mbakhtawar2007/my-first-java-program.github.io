d6cbf19ac7518228f578757dfc78723f1a7cce75
Muhammad Bakhtawar <m.bakhtawar.sce>
2024-02-15 20:39:50 +0500

    my first js program

---
+++ index.js
@@ -0,0 +1,13 @@
+document.write("2 x 1 = ",2*1, "<br>"); 
+document.write("2 x 2 = ",2*2, "<br>");
+document.write("2 x 3 = ",2*3, "<br>");
+document.write("2 x 4 = ",2*4, "<br>");
+document.write("2 x 5 = ",2*5, "<br>");
+document.write("2 x 6 = ",2*6, "<br>");
+document.write("2 x 7 = ",2*7, "<br>");
+document.write("2 x 8 = ",2*8, "<br>");
+document.write("2 x 9 = ",2*9, "<br>");
+document.write("2 x 10 = ",2*10, "<br>");
+
+
+
